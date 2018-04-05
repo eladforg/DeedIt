66fb95d441c5988ea3bc46e1f2b17bb900f8452f
Elad <elad.forgash@gmail.com>
2018-04-05 13:27:16 +0200

    adding nav bar + logo

--- app/app.js
+++ app/app.js
@@ -1,1 +1,24 @@
-var deeditApp = angular.module('deeditApp', []);
+var deeditApp = angular.module('deeditApp', ['ngRoute']);
+
+
+//creating router:
+// deeditApp.config(function($routeProvider){
+
+//     $routeProvider
+//     .when("/", {
+//         templateUrl:"app/home/home.html",
+//         controller:"homeCtrl"
+//     })
+//     .when("/gallery", {
+//         templateUrl:"app/dogsList/dogsList.html",
+//         controller:"dogsListCtrl"
+//     })
+//     .when("/chosendog/:breedName", {
+//         templateUrl:"app/chosenDogImages/chosenDogImages.html",
+//         controller:"chosenDogCtrl"
+//     })
+//     .otherwise({
+//         redirectTo:"/"
+//     })
+
+// });
