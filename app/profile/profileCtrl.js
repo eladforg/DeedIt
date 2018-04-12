bdcdf1f480e560dc560d14757ed495cd1602cbd3
eladforg <elad.forgash@gmail.com>
2018-04-12 15:22:49 +0300

    profile -start

--- app/profile/profileCtrl.js
+++ app/profile/profileCtrl.js
@@ -1,4 +1,7 @@
-deeditApp.controller('profileCtrl', function($scope){
+deeditApp.controller('profileCtrl', function($scope, activeUserSer, projectsSer){
 
+    //passing the user details:
+    $scope.userName=activeUserSer.activeUser.name;
+    $scope.userEmail=activeUserSer.activeUser.email;
 
 });
