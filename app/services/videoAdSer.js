97f039f84d2d9e5834a59ae089bb8f3715b1a9dd
Elad <elad.forgash@gmail.com>
2018-04-16 14:34:03 +0200

    videoads service

---
+++ app/services/videoAdSer.js
@@ -0,0 +1,26 @@
+// creating a service for requesting video ads:
+deeditApp.factory('videoAdSer', function(){
+
+    // an object that will have all the videoads URL:
+    var videoUrl = {
+        1:"app/videos/adopt.mp4",
+        2:"app/videos/dance.mp4",
+        3:"app/videos/dodge.mp4",
+        4:"app/videos/ikea.mp4",
+        5:"app/videos/isle.mp4",
+        6:"app/videos/parrot.mp4",
+        7:"app/videos/potato.mp4",
+        8:"app/videos/subaru.mp4"
+    };
+
+    // an array of all the videoads URL:
+    var videosArray = ["adopt","dance","dodge","ikea","isle","parrot","potato","subaru"];
+
+    
+
+
+    //access:
+    return {
+
+    }
+});
