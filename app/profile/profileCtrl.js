7565fe2b7c9fdca8924188d8a414beb965b5d35a
Elad <elad.forgash@gmail.com>
2018-04-23 23:08:55 +0200

    start of acordion at profile

--- app/profile/profileCtrl.js
+++ app/profile/profileCtrl.js
@@ -3,5 +3,7 @@ deeditApp.controller('profileCtrl', function($scope, activeUserSer, projectsSer)
     //passing the user details:
     $scope.userName=activeUserSer.activeUser.name;
     $scope.userEmail=activeUserSer.activeUser.email;
+    $scope.userGender=activeUserSer.activeUser.gender;
+    $scope.userAge=activeUserSer.activeUser.age;
 
 });
